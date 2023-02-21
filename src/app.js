4c5924db6d672d666786ffb175d63a3024864ba9
MartinM22 <martinmaldonado1999@gmail.com>
2023-02-21 00:37:50 +0000

    Falta mostrar domain generator en browser

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = generateDomain();
+  });
+};
+
+let generateDomain = () => {
+  var pronoun = ["the", "our", "her", "his", "theirs"];
+  var adj = ["great", "big", "small", "geek", "dangerous"];
+  var noun = ["jogger", "racoon", "cat", "car", "computer"];
+  var extension = [".com", ".net", ".us", ".io", ".co"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < extension.length; e++) {
+          //console.log(`${pronoun[p]}${adj[a]}${noun[n]}${extension[e]}`);
+          //let div = document.createElement("div");
+          //div.innerText = `${pronoun[p]}${adj[a]}${noun[n]}${extension[e]}`;
+          //document.body.appendChild(div);
+        }
+      }
+    }
+  }
 };
