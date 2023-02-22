d7671cb119cc16ca102a590a5db176a652dc8a35
MartinM22 <martinmaldonado1999@gmail.com>
2023-02-22 14:33:01 +0000

    Proyecto Listo

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let dom = document.querySelector("#domain");
+
 window.onload = () => {
   //write your code here
   document.querySelector("#button").addEventListener("click", () => {
-    document.querySelector("#domain").innerHTML = generateDomain();
+    dom.appendChild(generateDomain());
   });
 };
 
@@ -18,16 +20,18 @@ let generateDomain = () => {
   var noun = ["jogger", "racoon", "cat", "car", "computer"];
   var extension = [".com", ".net", ".us", ".io", ".co"];
 
+  let dominios = document.createElement("div");
   for (let p = 0; p < pronoun.length; p++) {
     for (let a = 0; a < adj.length; a++) {
       for (let n = 0; n < noun.length; n++) {
         for (let e = 0; e < extension.length; e++) {
-          //console.log(`${pronoun[p]}${adj[a]}${noun[n]}${extension[e]}`);
-          //let div = document.createElement("div");
-          //div.innerText = `${pronoun[p]}${adj[a]}${noun[n]}${extension[e]}`;
-          //document.body.appendChild(div);
+          console.log(`${pronoun[p]}${adj[a]}${noun[n]}${extension[e]}`);
+          let div = document.createElement("div");
+          div.innerText = `${pronoun[p]}${adj[a]}${noun[n]}${extension[e]}`;
+          dominios.appendChild(div);
         }
       }
     }
   }
+  return dominios;
 };
